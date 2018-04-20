859dc2b707367cd92b74a71a7fa7a005934612f6
Sara Reis <38558527+ReisSara8@users.noreply.github.com>
2018-04-20 15:21:33 -0300

    Add files via upload

---
+++ Exercicio17.java
@@ -0,0 +1,250 @@
+import javax.swing.JOptionPane;
+
+public class Exercicio17{
+
+public static void main(String[] args) {
+
+    int ano=Integer.parseInt(JOptionPane.showInputDialog("Digite o ano de seu nascimento"));
+    if (ano == 1900){
+        System.out.println("Mil e novecentos");
+    }else if (ano == 1901){
+        System.out.println("Mil novencentos e um");
+    }else if (ano == 1902){
+        System.out.println("Mil novencentos e dois");
+    }else if (ano == 1903){
+        System.out.println("Mil novencentos e três");
+    }else if (ano == 1904){
+        System.out.println("Mil novencentos e quatro");
+    }else if (ano == 1905){
+        System.out.println("Mil novencentos e cinco");
+    }else if (ano == 1906){
+        System.out.println("Mil novencentos e seis");
+    }else if (ano == 1907){
+        System.out.println("Mil novencentos e sete");
+    }else if (ano == 1908){
+        System.out.println("Mil novencentos e oito");
+    }else if (ano == 1909){
+        System.out.println("Mil novencentos e nove");
+    }else if (ano == 1910){
+        System.out.println("Mil novencentos e dez");
+    }else if (ano == 1911){
+        System.out.println("Mil novencentos e onze");
+    }else if (ano == 1912){
+        System.out.println("Mil novencentos e doze");
+    }else if (ano == 1913){
+        System.out.println("Mil novencentos e treze");
+    }else if (ano == 1914){
+        System.out.println("Mil novencentos e catorze");
+    }else if (ano == 1915){
+        System.out.println("Mil novencentos e quinze");
+    }else if (ano == 1916){
+        System.out.println("Mil novencentos e dezesseis");
+    }else if (ano == 1917){
+        System.out.println("Mil novencentos e dezessete");
+    }else if (ano == 1918){
+        System.out.println("Mil novencentos e dezoito");
+    }else if (ano == 1919){
+        System.out.println("Mil novencentos e dezenove");
+    }else if (ano == 1920){
+        System.out.println("Mil novencentos e vinte");
+    }else if (ano == 1921){
+        System.out.println("Mil novencentos e vinte e um");
+    }else if (ano == 1922){
+        System.out.println("Mil novencentos e vinte e dois");
+    }else if (ano == 1923){
+        System.out.println("Mil novencentos e vinte e três ");
+    }else if (ano == 1924){
+        System.out.println("Mil novencentos e vinte e quatro");
+    }else if (ano == 1925){
+        System.out.println("Mil novencentos e vinte e cinco");
+    }else if (ano == 1926){
+        System.out.println("Mil novencentos e vinte e seis");
+    }else if (ano == 1927){
+        System.out.println("Mil novencentos e vinte e sete");
+    }else if (ano == 1928){
+        System.out.println("Mil novencentos e vinte e oito");
+    }else if (ano == 1929){
+        System.out.println("Mil novencentos e vinte e nove");
+    }else if (ano == 1930){
+        System.out.println("Mil novencentos e trinta");
+    }else if (ano == 1931){
+        System.out.println("Mil novencentos e trinta e um");
+    }else if (ano == 1932){
+        System.out.println("Mil novencentos e trinta e dois");
+    }else if (ano == 1933){
+        System.out.println("Mil novencentos e trinta e três");
+    }else if (ano == 1934){
+        System.out.println("Mil novencentos e trinta e quatro");
+    }else if (ano == 1935){
+        System.out.println("Mil novencentos e trinta e cinco");
+    }else if (ano == 1936){
+        System.out.println("Mil novencentos e trinte e seis");
+    }else if (ano == 1937){
+        System.out.println("Mil novencentos e trinta e sete");
+    }else if (ano == 1938){
+        System.out.println("Mil novencentos e trinta e oito");
+    }else if (ano == 1939){
+        System.out.println("Mil novencentos e trinta e nove");
+    }else if (ano == 1940){
+        System.out.println("Mil novencentos e quarenta");
+    }else if (ano == 1941){
+        System.out.println("Mil novencentos e quarenta e um");
+    }else if (ano == 1942){
+        System.out.println("Mil novencentos e quarenta e dois");
+    }else if (ano == 1943){
+        System.out.println("Mil novencentos e quarenta e três");
+    }else if (ano == 1944){
+        System.out.println("Mil novencentos e quarenta e quatro");
+    }else if (ano == 1945){
+        System.out.println("Mil novencentos e quarenta e cinco");
+    }else if (ano == 1946){
+        System.out.println("Mil novencentos e quarenta e seis");
+    }else if (ano == 1947){
+        System.out.println("Mil novencentos e quarenta e sete");
+    }else if (ano == 1948){
+        System.out.println("Mil novencentos e quarenta e oito");
+    }else if (ano == 1949){
+        System.out.println("Mil novencentos e quarenta e nove");
+    }else if (ano == 1950){
+        System.out.println("Mil novencentos e cinquenta");
+    }else if (ano == 1951){
+        System.out.println("Mil novencentos e cinquenta e um");
+    }else if (ano == 1952){
+        System.out.println("Mil novencentos e cinquenta e dois");
+    }else if (ano == 1953){
+        System.out.println("Mil novencentos e cinquenta e três");
+    }else if (ano == 1954){
+        System.out.println("Mil novencentos e cinquenta e quatro");
+    }else if (ano == 1955){
+        System.out.println("Mil novencentos e cinquenta e cinco");
+    }else if (ano == 1956){
+        System.out.println("Mil novencentos e cinquenta e seis");
+    }else if (ano == 1957){
+        System.out.println("Mil novencentos e cinquenta e sete");
+    }else if (ano == 1958){
+        System.out.println("Mil novencentos e cinquenta e oito");
+    }else if (ano == 1959){
+        System.out.println("Mil novencentos e cinquenta e nove");
+    }else if (ano == 1960){
+        System.out.println("Mil novencentos e sessenta");
+    }else if (ano == 1961){
+        System.out.println("Mil novencentos e sessenta e um");
+    }else if (ano == 1962){
+        System.out.println("Mil novencentos e sessenta e dois");
+    }else if (ano == 1963){
+        System.out.println("Mil novencentos e sessenta e três");
+    }else if (ano == 1964){
+        System.out.println("Mil novencentos e sessenta e quatro");
+    }else if (ano == 1965){
+        System.out.println("Mil novencentos e sessenta e cinco");
+    }else if (ano == 1966){
+        System.out.println("Mil novencentos e sessenta e seis");
+    }else if (ano == 1967){
+        System.out.println("Mil novencentos e sessenta e sete");
+    }else if (ano == 1968){
+        System.out.println("Mil novencentos e sessenta e oito");
+    }else if (ano == 1969){
+        System.out.println("Mil novencentos e sessenta e nove");
+    }else if (ano == 1970){
+        System.out.println("Mil novencentos e setenta");
+    }else if (ano == 1971){
+        System.out.println("Mil novencentos e setenta e um");
+    }else if (ano == 1972){
+        System.out.println("Mil novencentos e setenta e dois");
+    }else if (ano == 1973){
+        System.out.println("Mil novencentos e setenta e três");
+    }else if (ano == 1974){
+        System.out.println("Mil novencentos e setenta e quatro");
+    }else if (ano == 1975){
+        System.out.println("Mil novencentos e setenta e cinco");
+    }else if (ano == 1976){
+        System.out.println("Mil novencentos e setenta e seis");
+    }else if (ano == 1977){
+        System.out.println("Mil novencentos e setenta e sete");
+    }else if (ano == 1978){
+        System.out.println("Mil novencentos e setenta e oito");
+    }else if (ano == 1979){
+        System.out.println("Mil novencentos e setenta e nove");
+    }else if (ano == 1980){
+        System.out.println("Mil novencentos e oitenta");
+    }else if (ano == 1981){
+        System.out.println("Mil novencentos e oitenta e um");
+    }else if (ano == 1982){
+        System.out.println("Mil novencentos e oitenta e dois");
+    }else if (ano == 1983){
+        System.out.println("Mil novencentos e oitenta e três");
+    }else if (ano == 1984){
+        System.out.println("Mil novencentos e oitenta e quatro");
+    }else if (ano == 1985){
+        System.out.println("Mil novencentos e oitenta e cinco");
+    }else if (ano == 1986){
+        System.out.println("Mil novencentos e oitenta e seis");
+    }else if (ano == 1987){
+        System.out.println("Mil novencentos e oitenta e sete");
+    }else if (ano == 1988){
+        System.out.println("Mil novencentos e oitenta e oito");
+    }else if (ano == 1989){
+        System.out.println("Mil novencentos e oitenta e nove");
+    }else if (ano == 1990){
+        System.out.println("Mil novencentos e noventa");
+    }else if (ano == 1991){
+        System.out.println("Mil novencentos e noventa e um");
+    }else if (ano == 1992){
+        System.out.println("Mil novencentos e noventa e dois");
+    }else if (ano == 1993){
+        System.out.println("Mil novencentos e noventa e três");
+    }else if (ano == 1994){
+        System.out.println("Mil novencentos e noventa e quatro");
+    }else if (ano == 1995){
+        System.out.println("Mil novencentos e noventa e cinco");
+    }else if (ano == 1996){
+        System.out.println("Mil novencentos e noventa e seis");
+    }else if (ano == 1997){
+        System.out.println("Mil novencentos e noventa e sete");
+    }else if (ano == 1998){
+        System.out.println("Mil novencentos e noventa e oito");
+    }else if (ano == 1999){
+        System.out.println("Mil novencentos e noventa e nove");
+    }else if (ano == 2000){
+        System.out.println("Dois mil");
+    }else if (ano == 2001){
+        System.out.println("Dois mil e um");
+    }else if (ano == 2002){
+        System.out.println("Dois mil e dois");
+    }else if (ano == 2003){
+        System.out.println("Dois mil e três");
+    }else if (ano == 2004){
+        System.out.println("Dois mil e quatro");
+    }else if (ano == 2005){
+        System.out.println("Dois mil e cinco");
+    }else if (ano == 2006){
+        System.out.println("Dois mil e seis");
+    }else if (ano == 2007){
+        System.out.println("Dois mil e sete");
+    }else if (ano == 2008){
+        System.out.println("Dois mil e oito");
+    }else if (ano == 2009){
+        System.out.println("Dois mil e nove");
+    }else if (ano == 2010){
+        System.out.println("Dois mil e dez");
+    }else if (ano == 2011){
+        System.out.println("Dois mil e onze");
+    }else if (ano == 2012){
+        System.out.println("Dois mil e doze");
+    }else if (ano == 2013){
+        System.out.println("Dois mil e treze");
+    }else if (ano == 2014){
+        System.out.println("Dois mil e catorze");
+    }else if (ano == 2015){
+        System.out.println("Dois mil e quinze");
+    }else if (ano == 2016){
+        System.out.println("Dois mil e dezesseis");
+    }else if (ano == 2017){
+        System.out.println("Dois mil e dezessete");
+    }else if (ano == 2018){
+        System.out.println("Dois mil e dezoito");
+    }
+
+}
+
+}
