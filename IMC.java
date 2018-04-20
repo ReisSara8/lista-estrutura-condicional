bbd6018e2fc33268c13120dda0bd484b344127cf
ReisSara8 <eusarareis16@gmail.com>
2018-04-20 08:46:07 -0300

    Inicialização do repositório

---
+++ IMC.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class IMC{
+
+    public static void main(String[] args) {
+        
+        double peso=Double.parseDouble(JOPtionPane.showInputDialog("Digite o seu peso"));
+        double altura
+    }
+    
+}
